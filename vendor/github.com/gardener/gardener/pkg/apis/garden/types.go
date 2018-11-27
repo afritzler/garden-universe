92e18022bcdd4297bda88f335aaafd85ed7ac008
Andreas Fritzler <andreas.fritzler@sap.com>
2018-11-27 18:27:44 +0100

    Bumped deps

--- vendor/github.com/gardener/gardener/pkg/apis/garden/types.go
+++ vendor/github.com/gardener/gardener/pkg/apis/garden/types.go
@@ -1200,11 +1200,14 @@ type Hibernation struct {
 
 // HibernationSchedule determines the hibernation schedule of a Shoot.
 // A Shoot will be regularly hibernated at each start time and will be woken up at each end time.
+// Start or End can be omitted, though at least one of each has to be specified.
 type HibernationSchedule struct {
 	// Start is a Cron spec at which time a Shoot will be hibernated.
-	Start string
+	// +optional
+	Start *string
 	// End is a Cron spec at which time a Shoot will be woken up.
-	End string
+	// +optional
+	End *string
 }
 
 // Kubernetes contains the version and configuration variables for the Shoot control plane.
@@ -1540,14 +1543,31 @@ const (
 	DefaultDomain = "cluster.local"
 )
 
+// ConditionStatus is the status of a condition.
+type ConditionStatus string
+
+// These are valid condition statuses. "ConditionTrue" means a resource is in the condition.
+// "ConditionFalse" means a resource is not in the condition. "ConditionUnknown" means kubernetes
+// can't decide if a resource is in the condition or not. "ConditionProgressing" means the condition was
+// seen true, failed but stayed within a predefined failure threshold. In the future, we could add other
+// intermediate conditions, e.g. ConditionDegraded.
+const (
+	ConditionTrue        ConditionStatus = "True"
+	ConditionFalse       ConditionStatus = "False"
+	ConditionUnknown     ConditionStatus = "Unknown"
+	ConditionProgressing ConditionStatus = "Progressing"
+)
+
 // Condition holds the information about the state of a resource.
 type Condition struct {
 	// Type of the Shoot condition.
 	Type ConditionType
 	// Status of the condition, one of True, False, Unknown.
-	Status corev1.ConditionStatus
+	Status ConditionStatus
 	// Last time the condition transitioned from one status to another.
 	LastTransitionTime metav1.Time
+	// Last time the condition was updated.
+	LastUpdateTime metav1.Time
 	// The reason for the condition's last transition.
 	Reason string
 	// A human readable message indicating details about the transition.
